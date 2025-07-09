9037dc5ea4a785f1f9b2ec671a4b6358fbcf4655
Mathias Elimz <mr9.3@icloud.com>
2025-07-09 12:11:26 -0400

    Initial commit - fresh Tabulero setup

---
+++ auth.js
@@ -0,0 +1,27 @@
+const auth = firebase.auth();
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+function signup() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCczDOZHTPPtD5rTIFlqDZX_G6pEWrvDcU",
+  authDomain: "client-portal-generator-fde6c.firebaseapp.com",
+  projectId: "client-portal-generator-fde6c",
+  storageBucket: "client-portal-generator-fde6c.firebasestorage.app",
+  messagingSenderId: "303527662714",
+  appId: "1:303527662714:web:f528380bfcde8b459e97c3",
+  measurementId: "G-QCLM2VK8W2"
+};
+
+firebase.initializeApp(firebaseConfig);
